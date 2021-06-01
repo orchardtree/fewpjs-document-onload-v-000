238f91a7165607814244ce7a7eb53e35fe0a7e6a
Jesse Wenger <wengerj@gmail.com>
2021-06-01 10:08:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 // Your code goes here
 document.addEventListener("DOMContentLoaded", function() {
-  console.log("The DOM has loaded");
+  msg = document.querySelector('p');
+  msg.textContent = 'This is really cool!'
 });
